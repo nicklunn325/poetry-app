0b576c9107ecfb299121747c170b6b99dc41b006
flatlandnick <nicholas.lunn@flatironschool.com>
2021-08-24 13:51:45 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
-  root "fallback#index"
   
   # namespace :api do
-    resources :users, only: [:create]
-    resources :poems, only: [:create, :index]
-    resources :categories, only: [:index]
-    
-    get '/me', to: 'users#show'
-    
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    # end 
-    
+  resources :users, only: [:create]
+  resources :poems, only: [:create, :index]
+  resources :categories, only: [:index]
+  
+  get '/me', to: 'users#show'
+  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  # end 
+  
+  root "fallback#index"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
