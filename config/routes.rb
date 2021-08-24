1ea0f00a2e05a3ea6b0eef50aeb5b78db9128029
flatlandnick <nicholas.lunn@flatironschool.com>
2021-08-24 11:27:38 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
-  resources :poems, only: [:create, :index]
-  resources :categories, only: [:index]
+  # namespace :api do
+    resources :users, only: [:create]
+    resources :poems, only: [:create, :index]
+    resources :categories, only: [:index]
 
-  get '/me', to: 'users#show'
+    get '/me', to: 'users#show'
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  # end 
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
